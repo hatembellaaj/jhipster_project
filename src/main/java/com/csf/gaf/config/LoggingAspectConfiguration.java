e0715a796aaabeb20be5514ba0818c640598766e
projet@five-consulting.com <projet@five-consulting.com>
2022-06-07 16:33:06 +0100

    first commit

---
+++ src/main/java/com/csf/gaf/config/LoggingAspectConfiguration.java
@@ -0,0 +1,17 @@
+package com.csf.gaf.config;
+
+import com.csf.gaf.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import tech.jhipster.config.JHipsterConstants;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
+    }
+}
---
+++ src/main/java/com/csf/gaf/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.csf.gaf.web.rest.vm;
